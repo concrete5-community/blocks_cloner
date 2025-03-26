7577787de3d2683711416c79b13de65aeb8d514d
Michele Locati <michele@locati.it>
2025-03-26 16:26:31 +0100

    Add converter for Theme Pixel

--- build/src/index.ts
+++ build/src/index.ts
@@ -3,11 +3,12 @@ import {setElementHighlighted} from './highlighter';
 import {hook as hookMenus} from './menu';
 import {getPageStructure} from './page-structure';
 import Core8To9Converter from './Conversion/Converter/Core8To9';
+import ThemePixel2To9Converter from './Conversion/Converter/ThemePixel2To9';
 import Conversion from './Conversion';
 import Xml from './Xml';
 
 if ((window as any).ccmBlocksCloner === undefined) {
-  Conversion.registerConverters([Core8To9Converter]);
+  Conversion.registerConverters([Core8To9Converter, ThemePixel2To9Converter]);
 
   (window as any).ccmBlocksCloner = {
     getPageStructure,
