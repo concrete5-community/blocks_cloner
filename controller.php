c37f39946db25153c17082e9053c5edd6787138f
Michele Locati <michele@locati.it>
2025-04-01 15:26:29 +0200

    Version 1.0.1

--- controller.php
+++ controller.php
@@ -21,7 +21,7 @@ class Controller extends Package implements ProviderAggregateInterface
 {
     protected $pkgHandle = 'blocks_cloner';
 
-    protected $pkgVersion = '1.0.0';
+    protected $pkgVersion = '1.0.1';
 
     /**
      * {@inheritdoc}
