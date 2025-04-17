5d44ac37a40b83979c55bd1b3ddf4da145c2ba31
Michele Locati <michele@locati.it>
2025-04-17 10:39:06 +0200

    Use DialogView for dialog views

--- src/Concrete/UI/Controller/Dialog.php
+++ src/Concrete/UI/Controller/Dialog.php
@@ -2,10 +2,24 @@
 
 namespace Concrete\Package\BlocksCloner\UI\Controller;
 
+use Concrete\Core\View\DialogView;
 use Concrete\Package\BlocksCloner\UI\Controller;
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
 abstract class Dialog extends Controller
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Package\BlocksCloner\UI\Controller::__construct()
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $view = new DialogView($this->viewPath);
+        $view->setPackageHandle('blocks_cloner');
+        $view->setController($this);
+        $this->setViewObject($view);
+    }
 }
