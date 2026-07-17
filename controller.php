d2c9351810d953634e62a64e4f81f15528683d19
Michele Locati <michele@locati.it>
2026-07-17 17:52:59 +0200

    Version 2.1.1
    Just for composer, no actual changes

--- controller.php
+++ controller.php
@@ -20,7 +20,7 @@ class Controller extends Package implements ProviderInterface
 {
     protected $pkgHandle = 'blocks_cloner';
 
-    protected $pkgVersion = '2.1.0';
+    protected $pkgVersion = '2.1.1';
 
     /**
      * {@inheritdoc}
