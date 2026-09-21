88ecf583082f92fa40d425f02c973c6323c571d6
Michele Locati <michele@locati.it>
2026-09-21 16:39:05 +0200

    Version 2.2.2

--- controller.php
+++ controller.php
@@ -20,7 +20,7 @@ class Controller extends Package implements ProviderInterface
 {
     protected $pkgHandle = 'blocks_cloner';
 
-    protected $pkgVersion = '2.2.1';
+    protected $pkgVersion = '2.2.2';
 
     /**
      * {@inheritdoc}
